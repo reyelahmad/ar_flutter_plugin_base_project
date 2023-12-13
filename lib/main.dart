af98737d914dd7213a2a2a325adff6e842f3fa48
Reyel Ahmad <79332437+reyelahmad@users.noreply.github.com>
2023-12-14 05:39:31 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:untitled/views/homwView.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme:
+          GoogleFonts.nunitoSansTextTheme(Theme.of(context).textTheme),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              centerTitle: true,
+              iconTheme: const IconThemeData(color: Colors.black),
+              titleTextStyle:
+              GoogleFonts.nunito(color: Colors.black, fontSize: 20))),
+      home: const MyHomePage(title: 'AR APP'),
+    );
+  }
+}
---
+++ lib/views/homwView.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:untitled/views/localAndWebObjectsView.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  const MyHomePage({required this.title, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LocalAndWebObjectsView()));
+              },
+              child: const Text("3D Objects")),
+        ));
+  }
+}
